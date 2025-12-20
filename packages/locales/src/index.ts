3d7923e32736b79c2c2b27510b681b0ce429122b
khalid999devs <khalidahammeduzzal@gmail.com>
2025-12-20 18:18:00 +0600

    updated

--- packages/locales/src/index.ts
+++ packages/locales/src/index.ts
@@ -1,6 +1,6 @@
 // Auto-generated file - DO NOT EDIT MANUALLY
 // Run 'pnpm generate:locales' to regenerate this file
-// Generated on: 2025-12-20T12:10:25.512Z
+// Generated on: 2025-12-20T12:17:52.243Z
 
 import bnCommon from './bn/common.json';
 import bnDashboard from './bn/dashboard.json';
--- packages/locales/src/types.ts
+++ packages/locales/src/types.ts
@@ -1,6 +1,6 @@
 // Auto-generated file - DO NOT EDIT MANUALLY
 // Run 'pnpm generate:locales' to regenerate this file
-// Generated on: 2025-12-20T12:10:25.511Z
+// Generated on: 2025-12-20T12:17:52.242Z
 
 import bnCommon from './bn/common.json';
 import bnDashboard from './bn/dashboard.json';
