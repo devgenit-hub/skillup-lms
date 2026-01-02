b48528afd40b10c3f8abe1f618f01555963613b1
khalid999devs <khalidahammeduzzal@gmail.com>
2026-01-02 23:23:32 +0600

    up

--- packages/locales/src/index.ts
+++ packages/locales/src/index.ts
@@ -1,6 +1,6 @@
 // Auto-generated file - DO NOT EDIT MANUALLY
 // Run 'pnpm generate:locales' to regenerate this file
-// Generated on: 2025-12-30T13:36:56.209Z
+// Generated on: 2026-01-02T17:14:31.567Z
 
 import bnCommon from './bn/common.json';
 import bnDashboard from './bn/dashboard.json';
